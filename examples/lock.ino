c20372aa3ff46541b54dea5a91bb281f90471ba0
el-crag <azertyggmf@outlook.com>
2024-08-13 01:33:35 -0500

    Fixed example and more verbose

--- examples/lock.ino
+++ examples/lock.ino
@@ -1,24 +1,50 @@
 #define PADLOCK_CLEAR_TEXT "clearTextToCipher"
+#define PADLOCK_CLEAR_TEXT_LENGTH 17
 
 #include "Padlock.hpp"
 
+#include <Arduino.h>
+
 Padlock * padlock = Padlock::getInstance();
 
+// Array of examples
+String pins[] = {"12345", "0123456789", "1234", "$", "1234 5", ""};
+
 void setup() {
+	Serial.begin(115200);
+
 	if (!padlock->hasSecurity()) {
-		padlock->pass("12345", 5, true);
+		// Use first element of pins as password
+		padlock->pass(pins[0].c_str(), pins[0].length(), true);
 	}
+
+	for (auto pin : pins) {
+		Serial.print("Testing '");
+		Serial.print(pin);
+		Serial.print("'...");
+		if (padlock->pass(pin.c_str(), pin.length())) {
+			Serial.print("  OK\n");
+		}
+		else {
+			Serial.print("  FAILED\n");
+		}
+	}
+
+	Serial.print("\nNow enter the pass and press enter:\n");
 }
 
 void loop() {
 	if (Serial.available() > 0) {
 		String pin = Serial.readString();
+		pin.trim(); // remove garbage
 
+		Serial.print("Entered: ");
+		Serial.print(pin);
 		if (padlock->pass(pin.c_str(), pin.length())) {
-			Serial.print("Passed\n");
+			Serial.print("  OK\n");
 		}
 		else {
-			Serial.print("Wrong\n");
+			Serial.print("  FAILED\n");
 		}
 	}
 }
